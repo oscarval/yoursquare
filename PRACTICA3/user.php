b00a41f5b7cb12ed48faf7bbde0a64b461d2f08a
Dominik Fleischmann <domfleischmann@gmail.com>
2017-04-27 16:43:49 +0200

    index y user hecho en php para aspecto basico

---
+++ PRACTICA3/user.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+      <head>
+      <title>
+      YourSquare
+      </title>
+      <meta charset="UTF-8">
+      <!-- Link CSS -->
+      <link href="https://fonts.googleapis.com/css?family=Monoton|PT+Sans" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet" type="text/css" />
+      </head>
+
+      <? php include cabecera.php>
+
+      <? php include usersidebar.php>
+
+      <? php include usercontent.php>
+
+      <? php include footer.php>
+
+      </body>
+      </html>
