9e8467e4bb5e03bd6f95ea32f722947d614d0edb
Dominik Fleischmann <domfleischmann@gmail.com>
2017-05-04 19:44:08 +0200

    vistas de admin

---
+++ PRACTICA3/views/adminsidebar.php
@@ -0,0 +1,24 @@
+<!-- sidebar izquierdo -->
+    <aside id="sidebar-left">
+     <p>Squares recientes</p>
+     <ul>
+     <li><a href="square_detail.html">Square1</a></li>
+     <li><a href="square_detail.html">Square2</a></li>
+     <li><a href="square_detail.html">Square3</a></li>
+     <li><a href="square_detail.html">+</a></li>
+     </ul>
+     <p>Squares menos valorados</p>
+     <ul>
+     <li><a href="square_detail.html">Square1</a></li>
+     <li><a href="square_detail.html">Square2</a></li>
+     <li><a href="square_detail.html">Square3</a></li>
+     <li><a href="square_detail.html">+</a></li>
+     </ul>
+     <p>Squares más valorados</p>
+     <ul>
+     <li><a href="square_detail.html">Square1</a></li>
+     <li><a href="square_detail.html">Square2</a></li>
+     <li><a href="square_detail.html">Square3</a></li>
+     <li><a href="square_detail.html">+</a></li>
+     </ul>
+     </aside>
