acbd8d8a41eb1121c36cd3fb499f739e6b5c3bf2
Xuebo <xuebozhu@ucm.es>
2017-05-21 14:56:58 +0200

    Add files via upload

--- PRACTICA4/views/usercontent.php
+++ PRACTICA4/views/usercontent.php
@@ -1,18 +1,27 @@
 <?php
-echo "<main id='main-withoutsidebar-right'>
-    <!-- <main id='main'> -->
-	  <section class='intro'>
-		<div class='container'>
-            <a href='square_detail.php'><div class='item'>1</div></a>
-            <a href='square_detail.php'><div class='item'>2</div></a>
-            <a href='square_detail.php'><div class='item'>3</div></a>
-            <a href='square_detail.php'><div class='item'>4</div></a>
-            <a href='square_detail.php'><div class='item'>5</div></a>
-            <a href='square_detail.php'><div class='item'>6</div></a>
-            <a href='square_detail.php'><div class='item'>7</div></a>
-            <a href='square_detail.php'><div class='item'>8</div></a>
-            <a href='square_detail.php'><div class='item'>9</div></a>
-        </div>
-	  </section>
-    </main>";
+include("../controller/Squares.php");
+$squares = new Squares();
+$squaresList = null;
+if(isset($_SESSION["login"])){
+  $squaresList = $squares->getSquaresUserLogin($_SESSION["id"], 10);
+  $html = "<a href='square_detail.php?id=%s'><div class='item'>%s usuario</div></a>";
+}else{
+  include("error_acceso.php");
+}
 ?>
+<main id='main-withoutsidebar-right'>
+    <!-- <main id='main'> -->
+		<section class='intro'>
+			<div class='container'>
+			<?php
+				if($squaresList){
+				  foreach($squaresList as $val){
+					echo sprintf($html,$val["sq_squareid"],$val["sq_title"]);
+				  }
+				}else{
+				  echo "<p>Sin Contenido</p>";
+				}
+			?>
+			</div>
+		</section>
+</main>
