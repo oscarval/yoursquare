54cd40abc9d05f03550626839a0092d7533a62ea
DomFleischmann <domfleischmann@gmail.com>
2017-05-30 19:17:20 +0200

    likes

--- PRACTICA4/views/likes.php
+++ PRACTICA4/views/likes.php
@@ -1,17 +1,21 @@
 <?php
+include_once("../controller/Squares.php");
 $id = $_REQUEST["id"];
 $mode = $_REQUEST["mode"];
 $square = new Squares();
 /* Hace falta hace un update square y devolver el valor nuevo de likes o dislikes depedniendo del modo en el que este*/
+$detail = $square->getSquareDetail($id);
 
 if($mode === "like"){
+	$newLike = (int) $detail['sq_likes'] + 1;
     $dataUpdate = [
-        "sq_likes" => "sq_likes + 1"
+        "sq_likes" => $newLike
     ];
     
 }else{
+	$newDislike = (int) $detail['sq_dislikes'] + 1;
     $dataUpdate = [
-        "sq_dislikes" => "sq_likes + 1"
+        "sq_dislikes" => $newDislike
     ];
 }
 $square->updateSquareDetails($id, $dataUpdate);
