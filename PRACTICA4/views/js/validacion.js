d3ba735ad13ef9de078cd6ab59e88c5ac51408ae
JuanCHL <jcleonardez@gmail.com>
2017-06-01 21:30:03 +0200

    Add files via upload

---
+++ PRACTICA4/views/js/validacion.js
@@ -0,0 +1,19 @@
+﻿$(function(){
+	$("#campoEmail").change(function(){
+		if ( correoValido($("#campoEmail").val() ) ) {
+		$("#noEmail").hide();
+		$("#okEmail").show();
+		} else {
+		$("#okEmail").hide();
+		$("#noEmail").show();
+		}
+	});
+	function correoValido(email) {
+	  if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(email)){
+	   return true;
+	  } else {
+	   return false;
+	  }
+	}
+});
+
