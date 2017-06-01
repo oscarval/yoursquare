99f428d93b054b370f21aa4961c76a6a18c16ebe
JuanCHL <jcleonardez@gmail.com>
2017-06-01 21:31:40 +0200

    Add files via upload

--- PRACTICA4/views/signUp.php
+++ PRACTICA4/views/signUp.php
@@ -2,42 +2,41 @@
 // página para el registro de un nuevo usuario
 include("cabecera.php");
 include("indexsidebar.php");
+echo '<link href="css/style_registrate.css" rel="stylesheet" type="text/css" />';
 ?>
 
-
-
-<form action="../controller/Procesar_signUp.php" method="post" id="signUp" >
-<h1 class="center">Únete a YOURSQUARE</h1>
-
-<div class="row-label">
-	<label>Sexo: </label>
-	<select name="sex">
-	        <option value="Indiferente">Indiferente</option>
-	        <option value="Masculino">Masculino</option>
-	        <option value="Femenino">Femenino</option>
-	</select>
-</div>
-
-<div class="row-label">
-  <label>Usuario: </label>
-  <input id="text_nombre" type="text" name="uname" placeholder="Introduce tu usuario">
-</div>
-<div class="row-label">
-  <label>Email: </label>
-  <input id="text_email" type="email" name="email" placeholder="Introduce tu email" required>
-</div>
-<div class="row-label">
-  <label>País: </label>
-  <input id="text_email" type="label" name="pais" placeholder="Introduce tu país" required>
-</div>
-<div class="row-label">
-  <label>Contraseña: </label>
-  <input id="text_email" type="password" name="psw" placeholder="Introduce tu contraseña" required>
-</div>
-<div class="row-label button_enviar center">
-  <input type="submit" value="Enviar" class="enviar">
-</div>
-</form>
+<main id="main-withoutsidebar-right">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css" rel="stylesheet" type="text/css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
+	<script type="text/javascript" src="js/validacion.js"></script>
+  <!-- <main id="main"> -->
+  <section class="intro">
+    <div>
+	<form action="../controller/Procesar_signUp.php" method="post" id="signUp" onsubmit="return validacion()" >
+	<h1 class="center">Únete a YOURSQUARE</h1>
+	<div class="row-label">
+	  <label>Usuario: </label>
+	  <input id="text_nombre" type="text" name="uname" placeholder="Introduce tu usuario" required>
+	</div>
+	<div class="row-label">
+	  <p><label>Correo:</label> <input type="text" name="campoEmail" id="campoEmail" /><img style="display:none" src="../img/ok.png" id="okEmail"/> <img style="display:none" src="../img/no.png" id="noEmail"/></p>
+	</div>
+	<div class="row-label">
+	<label>País: </label>
+	<input id="text_email" type="label" name="pais" placeholder="Introduce tu país" required>
+	</div>
+	<div class="row-label">
+	  <label>Contraseña: </label>
+	  <input id="text_password" type="password" name="psw" placeholder="Introduce tu contraseña" required>
+	</div>
+	<div class="row-label button_enviar center">
+	  <input type="submit" value="Registrarse" class="enviar">
+	</div>
+	</form>
+	</div>
+  </section>
+  </main>
 <?php
 include("footer.php");
 ?>
